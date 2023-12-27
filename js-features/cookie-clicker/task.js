1339321e7cb80416eea446716aedb662e384adcc
Alexandra Luka <lenimide@gmail.com>
2023-12-27 11:09:54 +0500

    2 task corrected

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,8 +1,10 @@
 const cookie = document.getElementById('cookie');
 const counter = document.getElementById('clicker__counter');
 const clickSpeed = document.getElementById('click__speed');
+let clickTime = Date.now();
 
 function changeSize() {
+	let timeDifference = Date.now() - clickTime;
 	cookie.width = '250';
 	cookie.height = '200';
 	counter.textContent = `${+counter.textContent + 1}`;
@@ -10,6 +12,8 @@ function changeSize() {
 		cookie.width = '200';
 		cookie.height = '150';
 	}, 100)
+	clickSpeed.textContent = `${(1 / timeDifference * 1000).toFixed(2)}`
+	clickTime = Date.now();
 }
 
 cookie.onclick = changeSize;
